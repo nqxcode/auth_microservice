5254b83005223491776e8b407c770e102824a185
Andrey Pokoev <avpokoyev@mts.ru>
2024-07-28 21:39:26 +0300

    week_2 WIP

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -2,34 +2,74 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
 	"log"
 	"net"
+	"time"
 
 	desc "github.com/nqxcode/auth_microservice/pkg/auth_v1"
+	"github.com/nqxcode/auth_microservice/pkg/hashing"
 
-	"github.com/brianvoe/gofakeit"
+	"database/sql"
+	sq "github.com/Masterminds/squirrel"
 	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/nqxcode/auth_microservice/internal/config"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-const grpcPort = 50051
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
+}
 
 type server struct {
 	desc.UnimplementedAuthV1Server
+	pool *pgxpool.Pool
 }
 
+var (
+	salt = "c2d7eae6c9914b6b176fdf91bd89c193"
+)
+
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	flag.Parse()
+	ctx := context.Background()
+
+	err := config.Load(configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	grpcConfig, err := config.NewGRPCConfig()
+	if err != nil {
+		log.Fatalf("failed to get grpc config: %v", err)
+	}
+
+	pgConfig, err := config.NewPGConfig()
+	if err != nil {
+		log.Fatalf("failed to get pg config: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	pool, err := pgxpool.Connect(ctx, pgConfig.DSN())
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterAuthV1Server(s, &server{})
+	desc.RegisterAuthV1Server(s, &server{pool: pool})
 
 	log.Printf("server listening at %v", lis.Addr())
 
@@ -39,34 +79,112 @@ func main() {
 }
 
 // Create user
-func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
 	log.Printf("User info: %v, password: %v, password confirm: %v", req.GetInfo(), req.GetPassword(), req.GetPasswordConfirm())
 
+	if req.Info == nil {
+		return nil, status.Error(codes.InvalidArgument, "info is required")
+	}
+
+	if req.Info.Name == "" {
+		return nil, status.Error(codes.InvalidArgument, "name is required")
+	}
+
+	if req.Info.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.Info.Role == 0 {
+		return nil, status.Error(codes.InvalidArgument, "role is required")
+	}
+
+	if req.Password != req.PasswordConfirm {
+		return nil, status.Error(codes.InvalidArgument, "passwords do not match")
+	}
+
+	password, err := hashing.HashPasswordWithSalt(req.Password, salt)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "could not hash password")
+	}
+
+	builderInsert := sq.Insert("\"user\"").
+		PlaceholderFormat(sq.Dollar).
+		Columns("name", "email", "role", "password").
+		Values(req.Info.Name, req.Info.Email, req.Info.Role, password).
+		Suffix("RETURNING user_id")
+
+	query, args, err := builderInsert.ToSql()
+	if err != nil {
+		log.Fatalf("failed to build query: %v", err)
+	}
+
+	var userID int
+	err = s.pool.QueryRow(ctx, query, args...).Scan(&userID)
+	if err != nil {
+		log.Fatalf("failed to insert user: %v", err)
+	}
+
+	log.Printf("inserted user with id: %d", userID)
+
 	return &desc.CreateResponse{
-		Id: int64(gofakeit.Number(1, 1000)),
+		Id: int64(userID),
 	}, nil
 }
 
 // Get user by id
-func (s *server) Get(_ context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
 	log.Printf("User id: %d", req.GetId())
+
+	builderSelect := sq.Select("user_id", "name", "email", "role", "created_at", "updated_at").
+		From("\"user\"").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"user_id": req.GetId()}).
+		Limit(1)
+
+	query, args, err := builderSelect.ToSql()
+	if err != nil {
+		log.Fatalf("failed to build query: %v", err)
+	}
+
+	rows, err := s.pool.Query(ctx, query, args...)
+	if err != nil {
+		log.Fatalf("failed to select user: %v", err)
+	}
+
+	var id int64
+	var name, email string
+	var role int32
+	var createdAt time.Time
+	var updatedAt sql.NullTime
+
+	if rows.Next() {
+		err = rows.Scan(&id, &name, &email, &role, &createdAt, &updatedAt)
+		if err != nil {
+			log.Fatalf("failed to scan user: %v", err)
+		}
+
+		log.Printf("id: %d, name: %s, email: %s, role: %d, created_at: %v, updated_at: %v\n", id, name, email, role, createdAt, updatedAt)
+	} else {
+
+		log.Printf("user not found")
+		return nil, status.Error(codes.NotFound, "user not found")
+	}
+
+	var outUpdatedAt *timestamppb.Timestamp
+	if updatedAt.Valid {
+		outUpdatedAt = timestamppb.New(updatedAt.Time)
+	}
 
 	return &desc.GetResponse{
 		User: &desc.User{
-			Id: req.GetId(),
+			Id: id,
 			Info: &desc.UserInfo{
-				Name:  gofakeit.Name(),
-				Email: gofakeit.Email(),
-				Role: func() desc.Role {
-					if gofakeit.Number(0, 1) == 0 {
-						return desc.Role_ADMIN
-					}
-
-					return desc.Role_USER
-				}(),
+				Name:  name,
+				Email: email,
+				Role:  desc.Role(role),
 			},
-			CreatedAt: timestamppb.New(gofakeit.Date()),
-			UpdatedAt: timestamppb.New(gofakeit.Date()),
+			CreatedAt: timestamppb.New(createdAt),
+			UpdatedAt: outUpdatedAt,
 		},
 	}, nil
 }
--- migrations/20230729123132_create_user_table.sql
+++ migrations/20230729123132_create_user_table.sql
@@ -4,7 +4,7 @@ create table "user"
     user_id    serial primary key,
     name       text      not null,
     email      text      not null,
-    role       text      not null,
+    role       integer   not null,
     password   text      not null,
     created_at timestamp not null default now(),
     updated_at timestamp
