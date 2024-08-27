003ea2c9179a69c66a96d2e644aeb76f9c36b017
Andrey Pokoev <avpokoyev@mts.ru>
2024-08-27 20:06:35 +0300

    week_4 WIP

--- internal/service/validator/validator.go
+++ internal/service/validator/validator.go
@@ -3,6 +3,7 @@ package validator
 type validator struct {
 }
 
+// ValidationError validation error
 type ValidationError struct {
 	message string
 }
@@ -12,10 +13,12 @@ func NewValidationError(message string) *ValidationError {
 	return &ValidationError{message: message}
 }
 
+// Error get error message
 func (e *ValidationError) Error() string {
 	return e.message
 }
 
+// NewValidator new validator
 func NewValidator() *validator {
 	return &validator{}
 }
