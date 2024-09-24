9c419042103fcf640e2c21f5e3306fdb17c6c061
Andrey Pokoev <avpokoyev@mts.ru>
2024-09-24 03:50:20 +0300

    week_6 WIP

--- internal/converter/role.go
+++ internal/converter/role.go
@@ -1,29 +1,29 @@
 package converter
 
 const (
-	UNKNOWN = 0
-	ADMIN   = 1
-	USER    = 2
+	UNKNOWN = "unknown"
+	ADMIN   = "admin"
+	USER    = "user"
 )
 
 func ToRoleString(role int32) string {
 	switch role {
-	case ADMIN:
-		return "admin"
-	case USER:
-		return "user"
+	case 1:
+		return ADMIN
+	case 2:
+		return USER
 	default:
-		return "unknown"
+		return UNKNOWN
 	}
 }
 
 func ToRole(role string) int32 {
 	switch role {
-	case "admin":
-		return ADMIN
-	case "user":
-		return USER
+	case ADMIN:
+		return 1
+	case USER:
+		return 2
 	default:
-		return UNKNOWN
+		return 0
 	}
 }
